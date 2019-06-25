490d6378c157339d51aa50c42a984b436d47d97d
Nandor Kraszlan <95nandi95@gmail.com>
2019-06-25 23:41:28 +0100

    Version bump

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,7 @@
-import Vue from "vue";
 import VueToastify from "./VueToastify.vue";
 
 export default {
-  install (Vue) {
+  install(Vue) {
     Vue.component("vue-toastify", VueToastify);
   }
-}
-
-export default { VueToastify };
+};
