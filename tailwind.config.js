dee2c2632fbb207e1c96d73e8d625b20d0466ee6
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2024-09-11 09:50:53 +0100

    remove (unused) wide glob pattern

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
     darkMode: "class",
-    content: ["**/*.html", "./src/**/*.{ts,tsx,vue}"],
+    content: ["./src/**/*.{ts,tsx,vue}"],
     plugins: [
         require("daisyui")
     ],
